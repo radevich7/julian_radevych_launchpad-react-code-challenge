17a1b98e8b13f515a3f35bd99742e2ed9142db1e
Julian Radevych <79290689+radevich7@users.noreply.github.com>
2021-12-23 17:20:34 -0700

    Build:universities page completed>

---
+++ src/Components/Universities/Tips.js
@@ -0,0 +1,63 @@
+const Tips = () => {
+  return (
+    <div className="tips_section">
+      <h1>How To Choose A University: 6 Tips</h1>
+      <h2>1. Make sure you’ve chosen the right subject </h2>
+      <p>
+        It is fundamental to be 120% sure about your subject. You will spend
+        your next three to six years studying it, and then the next 40-50 years
+        working in the area. If you have doubts about your choice, read as many
+        things about it as you can. Check job options, course contents,
+        salaries; Google is your friend. But you definitely do not want to
+        realize after two months of studying that you want to be a dentist
+        instead of a mathematician after all…{" "}
+      </p>
+
+      <h2>2. Consult university rankings </h2>
+      <p>
+        When I started searching for universities, I checked all the major
+        ranking tables and calculated averages. There are some which give you a
+        general ranking, while others may have separate tables for each subject.
+        For me, the most important data was the student:staff ratio. The lower
+        this ratio is, the better. In smaller classes you can ask questions and
+        understand the topics more easily.
+      </p>
+      <h2>3. Find out what the university library is like </h2>
+      <p>
+        One of the most important things is the library. You will spend a decent
+        amount of your life in the library, so it should be a comfortable
+        environment. Another good point to check is whether there’s a 24/7 cafè
+        for the early birds and night owls!
+      </p>
+      <h2>4. Check the course content</h2>
+      <p>
+        You can check the course content on the university website. If there’s
+        an area within the subject you’re particularly interested in, check
+        whether the university has it “on its menu” or not. Alternatively, you
+        can contact the university directly, and I am sure they will be more
+        than happy to answer any questions you have.
+      </p>
+      <h2>5. See what sports and societies are on offer</h2>
+      <p>
+        Whether you’re an avid footballer or love playing poker, you should
+        search for societies and clubs in the university to make sure there’s
+        something to match your hobby. All universities offer dozens (or
+        hundreds) of extra-curricular activities, and it’s important to check
+        these out too. University life is not just about studying!
+      </p>
+      <h2>6. Find out about the student accommodation</h2>
+      <p>
+        If you’re starting university, you will probably be moving away from
+        your family for the first time. Therefore it is essential that the
+        accommodation you live in is nice and friendly, somewhere you can call
+        your “home”. Be prepared: you may have to learn how to cook, how to use
+        the washing machine and how to do the washing up. And don’t worry about
+        the room-mates, flat-mates or floor-mates. In my experience it takes
+        approximately 2.5 days to be best friends with everyone in the shared
+        kitchen.
+      </p>
+    </div>
+  );
+};
+
+export default Tips;
--- src/Components/Universities/Universities.js
+++ src/Components/Universities/Universities.js
@@ -6,34 +6,188 @@ import {
   Dropdown,
   DropdownItem,
   DropdownMenu,
+  Col,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Card,
 } from "reactstrap";
 import CapAnimation from "../../animation/CapAnimation";
 import { useState } from "react/cjs/react.development";
 import Hamburger from "../../assets/svg/Hamburger";
+import ReusableButton from "../../ReusableComponents/ReusableButton";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import Tips from "./Tips";
 const Universities = () => {
   const [open, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!open);
+  //   Get flag emoji
+  const getFlagEmoji = (countryCode) =>
+    String.fromCodePoint(
+      ...[...countryCode.toUpperCase()].map((x) => 0x1f1a5 + x.charCodeAt())
+    );
+
   return (
     <Container className="container-xl pt-5 ">
-      <CapAnimation />
+      {/* <CapAnimation /> */}
       <Row className="d-flex position-relative " style={{ width: "100%" }}>
         <div className="students_img"> </div>
       </Row>
 
-      <Row className="dropdown_section d-flex justify-content-center p-5">
-        <Dropdown direction="end" toggle={toggle} isOpen={open}>
+      <Row className="dropdown_section d-flex justify-content-center p-5 ">
+        <Dropdown toggle={toggle} isOpen={open} direction="right">
           <DropdownToggle className="button_main dropdown_button">
-            Choose country
+            Choose region
             {/* Hamburger svg */}
             <Hamburger open={open} />
           </DropdownToggle>
-          <DropdownMenu end flip={false}>
-            <DropdownItem>Canada</DropdownItem>
-            <DropdownItem>Ukraine</DropdownItem>
-            <DropdownItem>USA</DropdownItem>
-            <DropdownItem>Mexico</DropdownItem>
+          <DropdownMenu className="dropDown_menu">
+            <Row>
+              <Col>
+                <DropdownItem>
+                  Canada <span className="flag"> {getFlagEmoji("ca")}</span>
+                </DropdownItem>
+                <DropdownItem>
+                  Ukraine<span className="flag"> {getFlagEmoji("ua")}</span>
+                </DropdownItem>
+                <DropdownItem>USA</DropdownItem>
+                <DropdownItem>Mexico</DropdownItem>
+                <DropdownItem>Mexico</DropdownItem>
+              </Col>
+              <Col>
+                <DropdownItem>
+                  Canada <span className="flag"> {getFlagEmoji("ca")}</span>
+                </DropdownItem>
+                <DropdownItem>
+                  Ukraine<span className="flag"> {getFlagEmoji("ua")}</span>
+                </DropdownItem>
+                <DropdownItem>USA</DropdownItem>
+                <DropdownItem>Mexico</DropdownItem>
+                <DropdownItem>Mexico</DropdownItem>
+              </Col>
+              <Col>
+                <DropdownItem>
+                  Canada <span className="flag"> {getFlagEmoji("ca")}</span>
+                </DropdownItem>
+                <DropdownItem>
+                  Ukraine<span className="flag"> {getFlagEmoji("ua")}</span>
+                </DropdownItem>
+                <DropdownItem>USA</DropdownItem>
+                <DropdownItem>Mexico</DropdownItem>
+                <DropdownItem>Mexico</DropdownItem>
+              </Col>
+              <Col>
+                <DropdownItem>
+                  Canada <span className="flag"> {getFlagEmoji("ca")}</span>
+                </DropdownItem>
+                <DropdownItem>
+                  Ukraine<span className="flag"> {getFlagEmoji("ua")}</span>
+                </DropdownItem>
+                <DropdownItem>USA</DropdownItem>
+                <DropdownItem>Mexico</DropdownItem>
+                <DropdownItem>Mexico</DropdownItem>
+              </Col>
+            </Row>
           </DropdownMenu>
         </Dropdown>
+      </Row>
+      <Row className="choice_section d-flex justify-content-center mb-5">
+        {/* <span className="choosen_countryFlag">{getFlagEmoji("ua")}</span> */}
+        <Col className="mb-3">
+          <Card className="universities_card">
+            <CardBody className="d-flex flex-column justify-content-around align-items-center">
+              <CardTitle tag="h5" className="universities_title">
+                University of Toronto
+              </CardTitle>
+
+              <CardText>web:'http://www.acadiau.ca/"'</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col className="mb-3">
+          <Card className="universities_card">
+            <CardBody className="d-flex flex-column justify-content-around align-items-center">
+              <CardTitle tag="h5" className="universities_title">
+                University of Toronto
+              </CardTitle>
+
+              <CardText>web:'http://www.acadiau.ca/"'</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col className="mb-3">
+          <Card className="universities_card">
+            <CardBody className="d-flex flex-column justify-content-around align-items-center">
+              <CardTitle tag="h5" className="universities_title">
+                University of Toronto
+              </CardTitle>
+
+              <CardText>web:'http://www.acadiau.ca/"'</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col className="mb-3">
+          <Card className="universities_card">
+            <CardBody className="d-flex flex-column justify-content-around align-items-center">
+              <CardTitle tag="h5" className="universities_title">
+                University of Toronto
+              </CardTitle>
+
+              <CardText>web:'http://www.acadiau.ca/"'</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col className="mb-3">
+          <Card className="universities_card">
+            <CardBody className="d-flex flex-column justify-content-around align-items-center">
+              <CardTitle tag="h5" className="universities_title">
+                University of Toronto
+              </CardTitle>
+
+              <CardText>web:'http://www.acadiau.ca/"'</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col className="mb-3">
+          <Card className="universities_card">
+            <CardBody className="d-flex flex-column justify-content-around align-items-center">
+              <CardTitle tag="h5" className="universities_title">
+                University of Toronto
+              </CardTitle>
+
+              <CardText>web:'http://www.acadiau.ca/"'</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col className="mb-3">
+          <Card className="universities_card">
+            <CardBody className="d-flex flex-column justify-content-around align-items-center">
+              <CardTitle tag="h5" className="universities_title">
+                University of Toronto
+              </CardTitle>
+
+              <CardText>web:'http://www.acadiau.ca/"'</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col className="mb-3">
+          <Card className="universities_card">
+            <CardBody className="d-flex flex-column justify-content-around align-items-center">
+              <CardTitle tag="h5" className="universities_title">
+                University of Toronto
+              </CardTitle>
+
+              <CardText>web:'http://www.acadiau.ca/"'</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        <ReusableButton style={"showMore_button mt-5 mb-5"}>
+          Show More Colleges
+        </ReusableButton>
+      </Row>
+      <Row>
+        <Tips />
       </Row>
     </Container>
   );
